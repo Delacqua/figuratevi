20065bdb2be67f3e7d51ce391be0a28820c3df1a
Delacqua <delacqua@gmail.com>
2019-05-28 22:56:35 +0200

    movimento slide

--- js/Slide.js
+++ js/Slide.js
@@ -3,35 +3,72 @@ class Slide {
   constructor(arrayImg) {
     this.arrayImg = arrayImg;
     this.doppioArray = Array.isArray(arrayImg[0]);
-    this.slideIndex = 1;
+    this.slideIndex = 0;
     this.mostraSlide(this.slideIndex);
   }
 
   cambiaSlide(n) {
-    this.mostraSlide(this.slideIndex += n);
+    let direction = "slide-left";
+    if (n>0) { direction = "slide-left"; }
+    if (n<0) { direction = "slide-right"; }
+    this.nextSlide(n, direction);
+    this.lastDirection = direction;
   }
+
+  nextSlide(n, direction) {
+
+    this.hiddenSlide( this.slideIndex, this.lastDirection);
+
+    this.slideIndex += n;
+
+    if (this.slideIndex > (this.arrayImg.length-1)) { this.slideIndex = 0}
+    if (this.slideIndex < 0) {this.slideIndex = (this.arrayImg.length-1)} ;
+
+    this.showSlide( this.slideIndex, direction);
+        
+  }
+
 
   mostraSlide(n) {
     let i;
-
-    if (n > this.arrayImg.length) { this.slideIndex = 1}
-    if (n < 1) {this.slideIndex = this.arrayImg.length} ;
-    for (i = 0; i < this.arrayImg.length; i++) {
-      if (this.doppioArray){
-        this.arrayImg[i][0].style.display = "none";
-        this.arrayImg[i][1].style.display = "none";
-      }
-      else {
-        this.arrayImg[i].style.display = "none";
-      }
-    }
     
-    if (this.doppioArray){
-        this.arrayImg[this.slideIndex-1][0].style.display = "block";
-        this.arrayImg[this.slideIndex-1][1].style.display = "block";
+    for (i = 0; i < this.arrayImg.length; i++) {
+        this.hiddenSlide( i, "slide-left");
+    }
+
+    this.showSlide(n, "slide-left");
+
+  }
+
+  hiddenSlide(index, direction) {
+      if (this.doppioArray){
+        this.arrayImg[index][0].style.display = "none";
+        this.arrayImg[index][0].classList.remove(direction);
+        this.arrayImg[index][1].style.display = "none";
+        this.arrayImg[index][1].classList.remove(direction);
       }
+
       else {
-        this.arrayImg[this.slideIndex-1].style.display = "block";
+        this.arrayImg[index].style.display = "none";
+        this.arrayImg[index].classList.remove(direction);
       }
-  }  
+
+  }
+
+  showSlide(index, direction) {
+      if (this.doppioArray){
+        this.arrayImg[index][0].style.display = "block";
+        this.arrayImg[index][0].classList.add(direction);
+        this.arrayImg[index][1].style.display = "block";
+        this.arrayImg[index][1].classList.add(direction);
+      }
+
+      else {
+        this.arrayImg[index].style.display = "block";
+        this.arrayImg[this.slideIndex].classList.add(direction);
+      }
+
+  }
+
+
 }
--- newsPage.php
+++ newsPage.php
@@ -20,47 +20,4 @@
         </div>
 
 
-<script type="text/javascript">
-
-    let slideIndex = 1;
-    var arrayImg = makeArrayImg();
-    mostraSlide(slideIndex);
-
-    
-
-    function cambiaSlide(n) {
-      mostraSlide(slideIndex += n);
-    }
-
-    function mostraSlide(n) {
-      let i;
-
-      if (n > arrayImg.length) { slideIndex = 1}
-      if (n < 1) {slideIndex = arrayImg.length} ;
-      for (i = 0; i < arrayImg.length; i++) {
-        arrayImg[i][0].style.display = "none";
-        arrayImg[i][1].style.display = "none";
-      }
-      
-      arrayImg[slideIndex-1][0].style.display = "block";
-      arrayImg[slideIndex-1][1].style.display = "block";
-    }
-
-    function makeArrayImg() {
-      let matches = document.querySelectorAll(".slidePerugia figure");
-      let matchesArray = Array.apply(null, matches);
-
-      let matchePair = matchesArray.reduce(function(result, value, index, array) {
-        if (index % 2 === 0)
-          result.push(array.slice(index, index + 2));
-        return result;
-      }, []);
-
-      return matchePair;
-
-    }
-
-</script>
-
-
 <?php get_footer(); ?>
