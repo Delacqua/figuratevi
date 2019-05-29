32fe3f9cef51cab1b210a7fe844b67816e6b314b
Delacqua <delacqua@gmail.com>
2019-05-29 21:38:20 +0200

    layout index per piccoli schemi

--- index.php
+++ index.php
@@ -5,44 +5,41 @@
 <?php $home = get_stylesheet_directory_uri();?>
 
 
-    <div class="container containerSfondo">
-        <div class="middle">
-            <div class="sinistra">
+    <div class="containerSfondo"> </div>
+        
+    <div class="index">
+        <div class="sinistra">
 
-                <div class="menuIndex menuIndexS">
-                    <a href="http://www.figuratevi.net/wordpress/valnerina/">
-                        <img src="<?= $home ?>/img/Mvalnerina.jpg">
-                    </a>
-                    
-                </div>
+            <div class="menuIndex menuIndexS">
+                <a href="http://www.figuratevi.net/wordpress/valnerina/">
+                    <img src="<?= $home ?>/img/Mvalnerina.jpg">
+                </a>
                 
             </div>
             
-            <div class="destra">
+        </div>
+        
+        <div class="destra">
 
-                <div class="menuIndex menuIndexD">
-                    <!--a href="http://www.figuratevi.net/wordpress/perugia/"-->
-                    <a href="<?= $home ?>/perugia/">
-                        <img src="<?= $home ?>/img/Mperugia.jpg">
-                    </a>
-                    
-                </div>
+            <div class="menuIndex menuIndexD">
+                <!--a href="http://www.figuratevi.net/wordpress/perugia/"-->
+                <a href="<?= $home ?>/perugia/">
+                    <img src="<?= $home ?>/img/Mperugia.jpg">
+                </a>
                 
             </div>
-
-            <div class="centro">
-                <a href="https://youtu.be/na20hxY9pp8" class="menuVideo" target="_blank">
-                    video
-                </a>
-            </div>
-
-            <div class="footerMenu">
-                <nav class="menuFooter" id="menuFooter"></nav>
-            </div>
-        
+            
         </div>
 
+        <div class="centro">
+            <a href="https://youtu.be/na20hxY9pp8" class="menuVideo" target="_blank">
+                video
+            </a>
+        </div>
+    
     </div>
+
+    
   
 
 <?php get_footer(); ?>
