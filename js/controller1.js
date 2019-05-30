3c7e1637984b1df924fd5d06dc2951928f4c3800
Delacqua <delacqua@gmail.com>
2019-05-30 20:57:56 +0200

    cambia numero di colonne sotto 400px (body)

--- js/controller1.js
+++ js/controller1.js
@@ -1,19 +1,23 @@
 /*  Controlle Page Slide  */
+let dom = document;
+let body = dom.getElementsByTagName('body')[0];
 
 var arrayImg = makeArrayImg();
 let slide = new Slide(arrayImg);
 
 function makeArrayImg() {
+  let slide; 
 
-  if (document.querySelectorAll(".slidePerugia").length) {
-      let matches = document.querySelectorAll(".slidePerugia figure");
+  if ((slide = dom.querySelectorAll(".slidePerugia")).length) {
+      resizeGallery(slide[0]);
+      let matches = slide[0].querySelectorAll("figure");
       let matchesArray = Array.apply(null, matches);
 
       return makePair(matchesArray);
   }
 
-  if (document.querySelectorAll(".slideValnerina").length) {
-      let matches = document.querySelectorAll(".slideValnerina figure");
+  if ((slide = dom.querySelectorAll(".slideValnerina")).length) {
+      let matches = slide[0].querySelectorAll("figure");
       let matchesArray = Array.apply(null, matches);
 
       return matchesArray;
@@ -31,6 +35,14 @@ function makePair(arr) {
       }, []);
 
   return matchePair;
+}
+
+function resizeGallery(gallery) {
+  if (body.clientWidth < 400 ) {
+      gallery.classList.remove("columns-2");
+      gallery.classList.add("columns-1");
+  }
+
 }
 
 
